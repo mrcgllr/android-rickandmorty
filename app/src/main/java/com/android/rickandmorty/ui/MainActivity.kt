d7392ea331238b1c30fb71c274cd5f13e2c02597
meric <mrcgllr95@gmail.com>
2022-05-03 20:52:19 +0300

    add main activity

--- app/src/main/java/com/android/rickandmorty/ui/MainActivity.kt
+++ app/src/main/java/com/android/rickandmorty/ui/MainActivity.kt
@@ -1,15 +1,17 @@
 package com.android.rickandmorty.ui
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.android.rickandmorty.R
 import com.android.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
